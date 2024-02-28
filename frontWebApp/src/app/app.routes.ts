1b234573040fdf1f7c8ccc4fd78de62a74a5b812
Juan Camilo Ramirez <juancamilorg64@gmail.com>
2024-02-28 03:14:08 +0000

    prototipo web adelanto

--- frontWebApp/src/app/app.routes.ts
+++ frontWebApp/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import {InicioSesionComponent} from './pages/inicio-sesion/inicio-sesion.component'
+import {RegistroComponent} from './pages/registro/registro.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: InicioComponent },
+    { path: 'inicio_sesion', component: InicioSesionComponent },
+    { path: 'registro', component: RegistroComponent }
+];
---
+++ frontWebApp/src/app/pages/inicio/inicio.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
+
+
+@Component({
+  selector: 'app-inicio',
+  standalone: true,
+  imports: [RouterLinkWithHref],
+  templateUrl: './inicio.component.html',
+  styleUrl: './inicio.component.scss'
+})
+export class InicioComponent {
+
+}
