615e3178cb1dfce836c88a5941dc60df24edf311
Juan Camilo Ramirez <juancamilorg64@gmail.com>
2024-03-02 04:19:11 +0000

    adelanto

--- frontWebApp/src/app/pages/servicios-terceros/servicios-terceros.component.ts
+++ frontWebApp/src/app/pages/servicios-terceros/servicios-terceros.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { SidebardComponent } from '../../shared/components/sidebard/sidebard.component';
+import { HorizontalCardComponent } from '../../shared/components/cards/horizontal-card/horizontal-card.component';
 @Component({
   selector: 'app-servicios-terceros',
   standalone: true,
-  imports: [HeaderComponent, SidebardComponent],
+  imports: [HeaderComponent, SidebardComponent, HorizontalCardComponent],
   templateUrl: './servicios-terceros.component.html',
   styleUrl: './servicios-terceros.component.scss'
 })
