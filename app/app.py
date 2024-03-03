fc8b5aef570233547dd7ea0dd3a3211ebe651bc6
Sebastian Arango <sebastian.x.arango.-nd@disney.com>
2024-03-03 17:34:05 -0500

    Add create user

--- app/app.py
+++ app/app.py
@@ -1,12 +1,15 @@
-from flask import Flask, jsonify, request
 import os
 
-from .models.router import Usuario 
+from flask import Flask, jsonify, request
+
 from .models.database import database
+from .models.router import Usuario
+
 app = Flask(__name__)
 
-
 API_KEY = os.getenv('API_KEY')
+
+
 # Ruta de salud para el health check
 @app.route('/', methods=['GET'])
 def health_check():
@@ -25,6 +28,23 @@ def obtener_usuarios():
             'username': user.username
         })
     return jsonify(users)
+
+
+# Ruta para crear usuarios
+@app.route('/usuario', methods=['POST'])
+def crear_usuario():
+    json = request.get_json()
+    usuario = Usuario(
+        nombre=json['nombre'],
+        email=json['email'],
+        username=json['username'],
+        password_hash=json['password_hash']
+    )
+    usuario_existe = Usuario.query.filter_by(username=json['username']).first()
+    if usuario_existe is None:
+        database.session.add(usuario)
+        database.commit()
+    return jsonify({'id': str(usuario.id)}), 201
 
 
 if __name__ == '__main__':
--- app/models/database.py
+++ app/models/database.py
@@ -1,8 +1,10 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
 from .router import db
-import os
-from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -14,7 +16,7 @@ class Database:
         try:
             self.create_tables()
         except:
-            pass  
+            pass
 
     def create_tables(self):
         db.metadata.create_all(self.engine)
@@ -22,12 +24,13 @@ class Database:
     def get_session(self):
         return self.Session()
 
+
 # Configuración de la base de datos
-DB_USER=os.getenv("DB_USER","example")
-DB_PASSWORD= os.getenv("DB_PASSWORD","example")
-DB_HOST=os.getenv("DB_HOST","localhost")
-DB_PORT=os.getenv("DB_PORT",5003)
-DB_NAME=os.getenv("DB_NAME","example")
+DB_USER = os.getenv("DB_USER", "example")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "example")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", 5003)
+DB_NAME = os.getenv("DB_NAME", "example")
 
 DB_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
