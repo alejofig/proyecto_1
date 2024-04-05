816042f58794eebfee044552e7f0c28402c6f6e4
Sebastian Arango <sebastian.x.arango.-nd@disney.com>
2024-04-05 18:46:41 -0500

    Desarrollo generar plan entrenamiento

--- frontWebApp/src/app/pages/generar-plan-entre/generar-plan-entre.component.ts
+++ frontWebApp/src/app/pages/generar-plan-entre/generar-plan-entre.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from '../../shared/components/header/header.component';
-import { CalendarComponent } from '../../shared/components/calendar/calendar.component';
-import { HorizontalCardComponent } from '../../shared/components/cards/horizontal-card/horizontal-card.component';
-import { SidebardComponent } from '../../shared/components/sidebard/sidebard.component';
-import { RouterLinkWithHref } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {HeaderComponent} from '../../shared/components/header/header.component';
+import {CalendarComponent} from '../../shared/components/calendar/calendar.component';
+import {HorizontalCardComponent} from '../../shared/components/cards/horizontal-card/horizontal-card.component';
+import {SidebardComponent} from '../../shared/components/sidebard/sidebard.component';
+import {RouterLinkWithHref} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {PlanEntrenamiento} from './plan-entrenamiento';
+import {PlanEntrenamientoService} from './plan-entrenamiento.service';
 
 @Component({
   selector: 'app-generar-plan-entre',
@@ -12,6 +15,35 @@ import { RouterLinkWithHref } from '@angular/router';
   templateUrl: './generar-plan-entre.component.html',
   styleUrl: './generar-plan-entre.component.scss'
 })
-export class GenerarPlanEntreComponent {
-  // hola
+export class GenerarPlanEntreComponent implements OnInit {
+
+  planEntrenamientoForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private planEntrenamientoService: PlanEntrenamientoService
+  ) {
+  }
+
+  ngOnInit() {
+    this.planEntrenamientoForm = this.formBuilder.group({
+      deporte: ["", [Validators.required]],
+      nombre: ["", [Validators.required]],
+      numeroEntrenamientosSemana: ["", [Validators.required]],
+      objetivoDistanciaEntrenamiento: ["", [Validators.required]],
+    })
+  }
+
+  generarPlanEntrenamiento(planEntrenamiento: PlanEntrenamiento) {
+    planEntrenamiento = new PlanEntrenamiento(
+      1,
+      "atletismo",
+      "juan",
+      1,
+      1)
+    this.planEntrenamientoService.generarPlanEntrenamiento(planEntrenamiento).subscribe((result) => {
+      console.info("El plan de entrenamiento fue generado", result)
+      this.planEntrenamientoForm.reset();
+    })
+  }
 }
