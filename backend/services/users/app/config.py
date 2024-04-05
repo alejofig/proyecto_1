3ba85f9f23a67c9d8130406ecf00db15de4b4e59
Alejandro Figueroa <alejofig@hotmail.com>
2024-04-05 12:00:40 -0500

    new tests (azul)

--- backend/services/users/app/config.py
+++ backend/services/users/app/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 DB_USER = os.getenv("DB_USER","prueba")
 DB_PASSWORD = os.getenv("DB_PASSWORD","prueba")
-DB_HOST = os.getenv("DB_HOST","users")
+DB_HOST = os.getenv("DB_HOST","localhost")
 DB_NAME = os.getenv("DB_NAME","prueba")
 DB_PORT = os.getenv("DB_PORT","5432")
 AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN")
