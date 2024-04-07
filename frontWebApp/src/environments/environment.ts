1f068121daf02566220612d381a309e71bda2300
Alejandro Figueroa <alejofig@hotmail.com>
2024-04-07 11:00:19 -0500

    Avances front

---
+++ frontWebApp/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  backendUrl: 'http://127.0.0.1:5000/',
+  auth0Domain: 'dev-s8qwnnguwcupqg2o.us.auth0.com/',
+  auth0ClientSecret: 'SnUDnO1lL3CnvzeCDFFUwwsFABY-Szfr-lRkFyshOf4uSnCiM6EHMgvCDDVQ8v1u',
+  auth0ClientId: '3H1DJStRDxr7jeKsxyvsPEe2Af8BpUcT',
+  auth0ApiIdentifier: 'https://dev-s8qwnnguwcupqg2o.us.auth0.com/api/v2/',
+  algorithm: 'RS256'
+};
