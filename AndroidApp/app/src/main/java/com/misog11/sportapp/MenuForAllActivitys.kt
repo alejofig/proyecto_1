93e9f0437dcaa7629f59fee282fe86a1790d3ad4
bemejia <b.mejial@uniandes.edu.co>
2024-04-09 16:46:02 -0500

    IMP_PFM-16_BEMEJIA:navegador de activitys

---
+++ AndroidApp/app/src/main/java/com/misog11/sportapp/MenuForAllActivitys.kt
@@ -0,0 +1,33 @@
+package com.misog11.sportapp
+
+import android.content.Intent
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+
+open class MenuForAllActivitys : AppCompatActivity() {
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.bottom_menu,menu)
+        return true
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.navigation_home -> print("home")
+            R.id.navigation_training -> navigate(EntrenamientoActivity::class.java)
+            R.id.navigation_events -> navigate(EventosActivity::class.java)
+
+        }
+        return true
+    }
+
+    private fun navigate(viewState:Class<*>){
+        val intent = Intent(this, viewState)
+        startActivity(intent)
+    }
+
+}
