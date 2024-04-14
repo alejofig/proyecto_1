b7c527adecbed15b14721a070c0173905679341c
bemejia <b.mejial@uniandes.edu.co>
2024-04-14 01:53:09 -0500

    IMP_PFM-16_BEMEJIA:Inicio retrofit metodo postEntrenamiento

--- AndroidApp/app/src/main/java/com/misog11/sportapp/models/Entrenamiento.kt
+++ AndroidApp/app/src/main/java/com/misog11/sportapp/models/Entrenamiento.kt
@@ -7,7 +7,7 @@ data class Entrenamiento (
     val id: Int,
     val user_id: Int,
     val sport_type: String,
-    val duration_horas: Double,
+    val duration: String,
     val fecha: Date,
     val calories_active: Int,
     val total_calories: Int,
