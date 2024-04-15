ec5a6fd7d08d9e8b86c0ab9ef656f59024bbd48e
Sebastian Arango <js.arangom1@uniandes.edu.co>
2024-04-15 18:51:11 -0500

    Edit planes

---
+++ backend/services/planes/tests/test_database.py
@@ -0,0 +1,22 @@
+from sqlite3 import IntegrityError
+
+from app.database import create_plan
+from app.models import Plan
+
+from create_plan import generate_random_plan
+
+
+def test_create_plan():
+    plan = Plan(**generate_random_plan())
+
+    try:
+        created_plan = create_plan(plan)
+    except IntegrityError as e:
+        assert False, f"Error al crear plan: {e}"
+
+    assert created_plan.deporte == plan.deporte
+    assert created_plan.nombre == plan.nombre
+    assert created_plan.usuario == plan.usuario
+    assert created_plan.cantidadEntrenamientos == plan.cantidadEntrenamientos
+    assert created_plan.distanciaPorEntrenamientos == plan.distanciaPorEntrenamientos
+    assert created_plan.fechas == plan.fechas
