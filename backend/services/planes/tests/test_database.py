ae937c0da1c305953920ee852f6b2f59349b620d
Sebastian Arango <js.arangom1@uniandes.edu.co>
2024-04-15 22:22:21 -0500

    Edit planes (azul)

--- backend/services/planes/tests/test_database.py
+++ backend/services/planes/tests/test_database.py
@@ -1,22 +1,9 @@
 from app.database import create_plan
 from app.models import Plan
-from sqlalchemy.exc import IntegrityError
 
 from create_plan import generate_random_plan
 
 
 def test_create_plan():
     plan = Plan(**generate_random_plan())
-
-    try:
-        created_plan = create_plan(plan)
-    except IntegrityError as e:
-        assert False, f"Error al crear plan: {e}"
-
-    # assert created_plan.id is not None
-    # assert created_plan.deporte == plan.deporte
-    # assert created_plan.nombre == plan.nombre
-    # assert created_plan.usuario == plan.usuario
-    # assert created_plan.cantidadEntrenamientos == plan.cantidadEntrenamientos
-    # assert created_plan.distanciaPorEntrenamientos == plan.distanciaPorEntrenamientos
-    # assert created_plan.fechas == plan.fechas
+    created_plan = create_plan(plan)
