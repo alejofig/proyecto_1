6e1cfe2b1943a8903cec57232b9c4c95f2fb012b
Alejandro Figueroa <alejofig@hotmail.com>
2024-04-21 18:22:32 -0500

    primeras pruebas cypress

--- backend/services/entrenamiento/app/models.py
+++ backend/services/entrenamiento/app/models.py
@@ -12,7 +12,6 @@ class Entrenamiento(SQLModel, table=True):
     calories_active: float = 0.0
     total_calories: float = 0.0
     fcm: int = 0
-    height: int = 0
     distance: int =  Field(default_factory=lambda: round(random.uniform(10, 100)))
 
 
