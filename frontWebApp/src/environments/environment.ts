033328cc38f105a731b09483c4092ce18d8bc441
Juan Camilo Ramirez <juancamilorg64@gmail.com>
2024-04-20 19:40:28 -0500

    IMP login eventos

--- frontWebApp/src/environments/environment.ts
+++ frontWebApp/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   auth0ApiIdentifier: 'https://dev-s8qwnnguwcupqg2o.us.auth0.com/api/v2/',
   algorithm: 'RS256',
   apigateway_url: 'http://127.0.0.1:3002',
-  planesUrl: 'https://planes.uniandes-sports.com',
-  eventos_urls: 'https://eventos.uniandes-sports.com',
+  planesUrl: 'https://apigateway.uniandes-sports.com',
+  eventos_urls: 'https://apigateway.uniandes-sports.com',
   local: 'http://3.88.143.81:3002'
 };
