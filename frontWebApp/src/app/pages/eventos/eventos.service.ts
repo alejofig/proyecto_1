1b4b322252945c000c3a1e02de48c5ed64d2e999
Sebastian Arango <js.arangom1@uniandes.edu.co>
2024-04-23 20:40:18 -0500

    Integrar sesiones entrenador con eventos

--- frontWebApp/src/app/pages/eventos/eventos.service.ts
+++ frontWebApp/src/app/pages/eventos/eventos.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {inject, Injectable} from '@angular/core';
 import {environment} from "../../../environments/environment";
-import { Observable } from 'rxjs';
-import { PlanEntrenamientoUser } from './eventos-interfaces';
+import {Observable} from 'rxjs';
+import {PlanEntrenamientoUser, SesionesEntrenador} from './eventos-interfaces';
 
 
 @Injectable({
@@ -12,16 +12,22 @@ export class EventosService {
 
   private http = inject(HttpClient)
   private eventosUrl: string = environment.eventos_urls;
-  private entrenamietoUrl: String = environment.planesUrl
+  private entrenamietoUrl: String = environment.planesUrl;
+  private entrenadorUrl: String = environment.tercerosUrl;
 
 
-  constructor() { }
+  constructor() {
+  }
 
-  getEvents(){
+  getEvents() {
     return this.http.get(this.eventosUrl + '/eventos');
   }
 
-  getEntrenamientos(): Observable<PlanEntrenamientoUser[]>{
+  getEntrenamientos(): Observable<PlanEntrenamientoUser[]> {
     return this.http.get<PlanEntrenamientoUser[]>(this.entrenamietoUrl + '/planes');
+  }
+
+  getSesionesEntrenador(): Observable<SesionesEntrenador[]> {
+    return this.http.get<SesionesEntrenador[]>(this.entrenadorUrl + '/sesiones_entrenador');
   }
 }
