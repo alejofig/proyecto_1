f8878468f1b8d2125a8b02915a2a5a0724c11057
Sebastian Arango <js.arangom1@uniandes.edu.co>
2024-04-26 14:28:53 -0500

    Tests terceros (azul)

--- backend/services/terceros/deploy.sh
+++ backend/services/terceros/deploy.sh
@@ -1,3 +1,4 @@
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 344488016360.dkr.ecr.us-east-1.amazonaws.com
 docker build -t servicio-terceros .
 docker tag servicio-terceros:latest 344488016360.dkr.ecr.us-east-1.amazonaws.com/servicio-terceros:latest
