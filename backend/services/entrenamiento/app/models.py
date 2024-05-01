90e8cca6f9cf5ef27c3bbd650aae83df6ad65a70
bemejia <b.mejial@uniandes.edu.co>
2024-05-01 13:06:21 -0500

    IMP_PFM-23_BEMEJIA:Inicio back rutinas

--- backend/services/entrenamiento/app/models.py
+++ backend/services/entrenamiento/app/models.py
@@ -2,6 +2,8 @@ import random
 from sqlmodel import Field, SQLModel
 from datetime import date
 from typing import Optional
+from pydantic import BaseModel
+
 
 class Entrenamiento(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -14,3 +16,13 @@ class Entrenamiento(SQLModel, table=True):
     fcm: int = 0
     distance: int =  Field(default_factory=lambda: round(random.uniform(10, 100)))
 
+
+class RutinaAlimenticia(BaseModel):
+    carboidratos: float
+    proteinas: float
+    grasas: float
+
+class RutinaDescanso(BaseModel):
+    minutos_sueno: int
+    minutos_meditacion: int
+    minutos_relajacion: int 
